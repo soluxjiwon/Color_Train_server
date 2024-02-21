20ad68585a04e27d7f3ff168748039b3005a9447
soluxjiwon <are00011357@gmail.com>
2024-02-22 01:42:29 +0900

    .

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const config=require('./config/dev');
 //app.use(cors());
 //cors설정한 부분
 app.use(cors({
-  origin:true, // 출처 허용 옵션
+  //origin:true, // 출처 허용 옵션
+  origin:"https://localhost:3000",
   credentials:true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 }));
 
