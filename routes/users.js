c1b82f823a2c86b7ef04924af12b8c283bbc8555
soluxjiwon <are00011357@gmail.com>
2024-02-22 18:21:11 +0900

    .

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const { User } = require("../models/User");
 const { auth } = require("../middleware/auth");
 
 //router 설정
-/*router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://web-colortrain-client-am952nlsu6unuj.sel5.cloudtype.app/");
     res.header("Access-Control-Allow-Credentials", true);  
     res.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=None") 
 
     next();
-    });*/
+    });
 
 //=================================
 //             User
